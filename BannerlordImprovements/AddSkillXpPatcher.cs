1fc5e657974d7e44ff70e064a7d53c87a52e6dec
sargon <statkevich.ivan999@gmail.com>
2022-07-16 19:21:09 +0300

    Добавьте файлы проекта.

---
+++ BannerlordImprovements/AddSkillXpPatcher.cs
@@ -0,0 +1,18 @@
+﻿
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+
+namespace BannerlordImprovements.Patches
+{
+    [HarmonyPatch(typeof(Hero), "AddSkillXp")]
+    public class AddSkillXpPatcher
+    {
+        private static void Prefix(Hero __instance, SkillObject skill, float xpAmount)
+        {
+            Hero hero = __instance;
+            float newXpAmount = xpAmount * 20;
+            hero.HeroDeveloper.AddSkillXp(skill, newXpAmount, true, true);
+        }
+    }
+}
---
+++ BannerlordImprovements/MySubModule.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace BannerlordImprovements
+{
+    public class MySubModule: MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            var campaign = game.GameType as Campaign;
+            if (campaign == null)
+            {
+                return;
+            }
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            try
+            {
+                new Harmony("mod.bannerlord.bannerlordimprovements").PatchAll();
+            }
+            catch (Exception ex)
+            {
+                InformationManager.DisplayMessage(new InformationMessage(ex.ToString()));
+            }
+        }
+    }
+}
